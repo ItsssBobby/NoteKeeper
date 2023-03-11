2cec49f1205542d7e827024365e9edea5f12246b
ItsssBobby <114122721+ItsssBobby@users.noreply.github.com>
2023-03-11 18:01:32 -0500

    fixed routes - post/get/delete all functional

--- server.js
+++ server.js
@@ -8,10 +8,8 @@ const htmlRoutes = require('./routes/htmlRoutes');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
-
-// routes once created
 app.use('/api', apiRoutes);
-// app.use('/', htmlRoutes);
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
